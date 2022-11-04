bfe8f0ffd0b1cce57614300d148c5c3e9227a3ef
Shindepoonam <116255804+Shindepoonam@users.noreply.github.com>
2022-11-04 17:52:13 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About = props => {
+  return (
+    <body>
+
+    <div>
+      <h1>About</h1>
+      <p> Welcome to the about page</p>
+    </div>
+    
+    </body>
+  );
+};
+
+export default About;
