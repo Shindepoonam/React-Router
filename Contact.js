824e585b70301fe0dcbad04754d07d1081330aa7
Shindepoonam <116255804+Shindepoonam@users.noreply.github.com>
2022-11-04 17:52:40 +0530

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = props => {
+  return (
+    <div>
+      <h3>Contact</h3>
+      <p>Welcom to the contact page</p>
+    </div>
+  );
+};
+
+export default Contact;
